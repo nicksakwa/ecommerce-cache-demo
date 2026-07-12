7c5df8273a0dfecd913988b2812e4eeabd7501dd
nicksakwa <nsakwa@gmail.com>
2026-07-12 20:00:29 +0300

    indexed update to db.ts

--- frontend/src/utils/db.ts
+++ frontend/src/utils/db.ts
@@ -2,13 +2,13 @@
 export const openIndexedDB = (): Promise<IDBDatabase> => {
     return new Promise((resolve, reject) => {
         const request = indexedDB.open("EcommerceDB", 1);
-        request.onupgradeneeded = (event) => {
+        request.onupgradeneeded = () => {
             const db = request.result;
             if (!db.objectStoreNames.contains("products")){
                 db.createObjectStore("products", { keyPath: "id"});
             }
         };
-        request.onsuccess = (event) => resolve(request.result);
-        request.onerror = (event) => reject (request.error);
+        request.onsuccess = () => resolve(request.result);
+        request.onerror = () => reject(request.error);
     });
 };
