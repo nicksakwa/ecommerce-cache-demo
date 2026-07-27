3342b799d2ffffd69f049968fa814e1051748055
nicksakwa <nsakwa@gmail.com>
2026-07-27 18:24:59 +0300

    import redux,indexdb,redux and lazy load heavy her0

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,3 +3,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchProductsViaRedux } from '.store/store';
 import { ProductList } from './components/ProductList';
 import { openIndexedDB } from './utils/db';
+
+const HeavyHero = lazy(()=> import('./components/HeavyHero'));
+const localInMemoryCache: Record<string, any> ={};
+export default function App(){
+    const dispatch = useDispatch(<any>();
+}
